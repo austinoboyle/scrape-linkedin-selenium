7e6997eba3ba0f2a90361a82be99d43cc19c3c21
austinoboyle <14arob@queensu.ca>
2018-06-01 15:46:52 -0400

    Add MyConnectionScraper - scrape all your own connections

--- scrape_linkedin/__init__.py
+++ scrape_linkedin/__init__.py
@@ -5,3 +5,4 @@ from .ParallelScraper import scrape_in_parallel
 from .Company import Company
 from .CompanyScraper import CompanyScraper
 from .ConnectionScraper import ConnectionScraper
+from .MyConnectionScraper import MyConnectionScraper
