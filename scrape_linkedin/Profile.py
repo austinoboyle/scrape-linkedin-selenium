0c3f7306fe4bfd178e33ea0accd0f8dbf2c9b0d2
Kireet Agrawal <kireet.agrawal@thoughtspot.com>
2021-05-10 18:03:11 -0700

    fixes

--- scrape_linkedin/Profile.py
+++ scrape_linkedin/Profile.py
@@ -25,10 +25,8 @@ class Profile(ResultsObject):
             'school': 'li[data-control-name="education_see_more"]',
             'location': '.pv-top-card--list-bullet > li',
         })
-        print(personal_info)
         
-
-        if "connections" in personal_info.get('name'):
+        if "connections" in personal_info.get('name') or personal_info.get('name') == None:
             personal_info = get_info(top_card, {
                 'name': '.text-heading-xlarge',
                 'headline': '.text-body-medium',
@@ -36,7 +34,6 @@ class Profile(ResultsObject):
                 'school': 'li[data-control-name="education_see_more"]',
                 'location': '.text-body-small',
             })
-        print(personal_info)
 
         personal_info['summary'] = text_or_default(
             self.soup, '.pv-about-section .pv-about__summary-text', '').replace('... see more', '').strip()
